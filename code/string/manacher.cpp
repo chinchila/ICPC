fa29b3447444dcaa059deede825a8a3b385f93a8
Chinchila <pachecosobral@gmail.com>
2020-01-08 21:01:44 -0300

    manacher com impar e par adicionado

--- code/string/manacher.cpp
+++ code/string/manacher.cpp
@@ -1,3 +1,26 @@
+// d1, d2 = number of palindromes with odd and even lengths with centers in i
+vector<int> d1, d2;
+
+void manacher( string s ){
+	int n = s.length();
+	// odd
+	d1.resize(n);
+	for (int i = 0, l = 0, r = -1; i < n; i++) {
+		int k = (i > r) ? 1 : min(d1[l + r - i], r - i + 1);
+		while (0 <= i - k && i + k < n && s[i - k] == s[i + k]) k++;
+		d1[i] = k--;
+		if (i + k > r) l = i - k, r = i + k;	
+	}
+	// even
+	d2.resize(n);
+	for (int i = 0, l = 0, r = -1; i < n; i++) {
+		int k = (i > r) ? 0 : min(d2[l + r - i + 1], r - i + 1);
+		while (0 <= i - k - 1 && i + k < n && s[i - k - 1] == s[i + k]) k++;	
+		d2[i] = k--;
+		if (i + k > r) l = i - k - 1, r = i + k ;
+	}
+}
+
 // To get the string just str.substr( ( id + 1 - mx ) / 2, mx ) | mx is the size of the LPS
 // O(n)
 pair<int, int> manacher( string str ){
@@ -12,4 +35,5 @@ pair<int, int> manacher( string str ){
 	pal.pop_back();
 	return { mx, id };
 }
+
 
