6986669337040a78dbde08716d77025ff62319fd
Chinchila <pachecosobral@gmail.com>
2020-02-02 23:00:40 -0300

    Fast kuhn instead of slow

--- code/graphs/kuhn.cpp
+++ code/graphs/kuhn.cpp
@@ -1,22 +1,31 @@
-// Maximum cardinality (bipartite matching) O(n^3) worst case, if slow random_shuffle vertice orders.
+// Maximum cardinality (bipartite matching) O(n^3) worst case
+// if slow random_shuffle vertice orders.
 // Apply it only on left set. indexed 1
-vector<int> g[MAX_N];
-int vis[MAX_N], b[MAX_N];
+vector<int> g[MAXN];
+int vis[MAXN], ma[MAXN], mb[MAXM];
 int n, x; // n is size of left set
 
-bool dfs( int u )
-{
-	if( vis[u] == x ) return 0;
-	vis[u] = x;
-	for( int v : g[u] ) if( !b[v] || dfs( b[v] ) )
-		return b[v] = u;
+bool dfs( int u ) {
+	for( int v : g[u] ) if(vis[v] != x) {
+		vis[v] = x;
+		if( mb[v] == -1 || dfs( mb[v] ) ) {
+			mb[v] = u, ma[u] = v;
+			return 1;
+		}
+	}
 	return 0;
 }
- 
-int kuhn()
-{
+
+int kuhn() {
+	memset(ma, -1, sizeof(ma));
+	memset(mb, -1, sizeof(mb));
+	bool aux = 1;
 	int ans = 0;
-	for( int i = 1 ; i <= n ; ++i ) ++x, ans += dfs( i );
+	while( aux ) {
+		++x, aux = 0;
+		for( int i = 1 ; i <= n ; ++i )
+			if( ma[i] == -1 && dfs(i) ) ++ans, aux = 1;
+	}
 	return ans;
 }
 
