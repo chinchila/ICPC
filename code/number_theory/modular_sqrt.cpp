096ce94f46a65076b0c5d248270761020a5a03fa
Chinchila <pachecosobral@gmail.com>
2020-10-04 16:28:47 -0300

    modular square root - tonelli shanks

---
+++ code/number_theory/modular_sqrt.cpp
@@ -0,0 +1,51 @@
+// do not forget fastexp on numeric_fundamentals.cpp
+// be careful to use mulmod when n and p are big.
+#define ll long long
+bool modsqrt( ll n, ll p, ll &root1, ll &root2 ){
+	ll q = p - 1;
+	ll ss = 0;
+	ll z = 2;
+	ll c, r, t, m;
+
+	if( fastexp(n, (p - 1) / 2, p) != 1 ){
+		root1 = 0;
+		root2 = 0;
+		return false;
+	}
+
+	while ((q & 1) == 0)
+		ss += 1, q >>= 1;
+ 
+	if( ss == 1 ){
+		root1 = fastexp(n, (p + 1) / 4, p);
+        root2 = p - root1;
+		return true;
+	}
+ 
+	while( fastexp(z, (p - 1) / 2, p) != p - 1 )
+		++z;
+ 
+	c = fastexp(z, q, p);
+	r = fastexp(n, (q + 1) / 2, p);
+	t = fastexp(n, q, p);
+	m = ss;
+ 
+	while( true ){
+		ll i = 0, zz = t;
+		ll b = c, e;
+		if (t == 1) {
+            root1 = r;
+            root2 = p - r;
+			return true;
+		}
+		while (zz != 1 && i < (m - 1))
+			zz = zz * zz % p, ++i;
+		e = m - i - 1;
+		while( e > 0 )
+			b = b * b % p, --e;
+		r = r * b % p;
+		c = b * b % p;
+		t = t * c % p;
+		m = i;
+	}
+}
