a9ebb875493c5134bf038624da35945b2a86c563
Chinchila <pachecosobral@gmail.com>
2020-11-30 21:46:12 -0300

    Number of occurences of substrings

--- code/string/suffixautomata.cpp
+++ code/string/suffixautomata.cpp
@@ -1,5 +1,8 @@
 // Suffix Automaton Construction - O(n) FROM IME
 // Suffix automaton = compressed form of all substrings
+// len[i] = # of substrings that pass through state i
+// sl[i] = suffix link of state i
+// sz = 
 const int N = 1e6+1, K = 26;
 int sl[2*N], len[2*N], sz, last;
 ll cnt[2*N];
@@ -9,11 +12,9 @@ void add(int c) {
 	int u = sz++;
 	len[u] = len[last] + 1;
 	cnt[u] = 1;
-
 	int p = last;
 	while(p != -1 and !adj[p][c])
 		adj[p][c] = u, p = sl[p];
-
 	if (p == -1) sl[u] = 0;
 	else {
 		int q = adj[p][c];
@@ -28,7 +29,6 @@ void add(int c) {
 			sl[q] = sl[u] = r;
 		}
 	}
-
 	last = u;
 }
 
@@ -91,6 +91,9 @@ ll substr_cnt() {
 // Number of occurences - O(|p|)
 vector<int> t[2*N];
 
+// This count the occurrences of each state
+// to find the number of occurences of substrings
+// use cnt[i]*(len[i] - len[sl[i]])
 void occur_count(int u) {
 	for(int v : t[u]) occur_count(v), cnt[u] += cnt[v];
 }
