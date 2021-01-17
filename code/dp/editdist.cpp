af16d37d96ae601a08e37f4d44bf8fb789d45a9a
Chinchila <pachecosobral@gmail.com>
2021-01-17 18:38:15 -0300

    Edit distance certo

--- code/dp/editdist.cpp
+++ code/dp/editdist.cpp
@@ -5,8 +5,8 @@
 int editDistDP( string s1, string s2 ){
 	int m = s1.size(), n = s2.size();
 	int dp[m+1][n+1];
-	for( int i = 0 ; i <= n ; ++i ){
-		for( int j = 0 ; j <= m ; ++j ){
+	for( int i = 0 ; i <= m ; ++i ){
+		for( int j = 0 ; j <= n ; ++j ){
 			if( i == 0 ) dp[i][j] = j;
 			else if( j == 0 ) dp[i][j] = i;
 			else if( s1[i-1] == s2[j-1] )
@@ -16,6 +16,6 @@ int editDistDP( string s1, string s2 ){
 				dp[i][j] = 1 + min( dp[i][j-1], min( dp[i-1][j], dp[i-1][j-1] ) );
 		}
 	}
-	return dp[n][m];
+	return dp[m][n];
 }
 
