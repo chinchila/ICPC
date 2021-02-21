5f3cac045674d00ea1ade757eb17f5d85f142628
João Sobral <pachecosobral@gmail.com>
2021-02-21 10:30:40 -0300

    Maximum clique + correct prime factors description

--- code/number_theory/prime_factors.cpp
+++ code/number_theory/prime_factors.cpp
@@ -1,12 +1,11 @@
-// Find all prime factors | O(n^1/3) ?
+// Find prime factors, doesn't find prime itself | O(n^1/3)
+// can be used to count divisors and when we need product of primes
 // here we find the smallest finite base of a fraction a/b
 #define ll long long
 int main() {
     scanf("%lld %lld", &a, &b);
- 
     ll g = __gcd(a, b);
     b /= g;
- 
     cur = b;
     for(ll i = 2; i <= cbrt(cur); i++) {
         if(cur % i == 0) {
@@ -14,10 +13,8 @@ int main() {
             while(cur % i == 0) cur /= i;
         }
     }
- 
     ll sq = round(sqrt(cur));
     if(sq * sq == cur) cur = sq;
- 
     printf("%lld\n", max(2LL, ans * cur));
     return 0;
 }
