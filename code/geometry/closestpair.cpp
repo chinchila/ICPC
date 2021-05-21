8b360a271d0f2ffdbb798552dac1465db9d34f65
João Sobral <pachecosobral@gmail.com>
2021-05-20 22:43:51 -0300

    Closest pair corrigido

--- code/geometry/closestpair.cpp
+++ code/geometry/closestpair.cpp
@@ -1,27 +1,28 @@
 // Closest pair with line sweep
+// retorna quadrado da menor distancia
 // O(n log n)
 #define ll long long
 #define nd second
 #define st first
-int n; //amount of points
-pair<ll, ll> pnt[MAXN];
 
-struct cmp{
-	bool operator()(pair<ll,ll> a, pair<ll, ll> b) { return a.nd < b.nd; }
-};
-
-double closest_pair() {
-	sort( pnt, pnt + n );
-	double best = numeric_limits<double>::infinity();
-	set<pair<ll, ll>, cmp> box;
-	box.insert( pnt[0] );
+ll closest_pair(vector<pair<ll, ll>> &pnt) {
+	sort( pnt.begin(), pnt.end() );
+	ll best = numeric_limits<ll>::max();
+	ll d = ceil(sqrt(best));
+	set<pair<ll, ll>> box;
+	box.insert( {pnt[0].nd, pnt[0].st} );
 	int l = 0;
-	for( int i = 1 ; i < n ; ++i ){
-		while( l < i && pnt[i].st - pnt[l].st > best )
-			box.erase( pnt[l++] );
-		for( auto it = box.lower_bound( {0, pnt[i].nd - best} ) ; it != box.end() && pnt[i].nd + best >= it->nd ; ++it )
-			best = min( best, hypot( pnt[i].st - it->st, pnt[i].nd - it->nd ) );
-		box.insert( pnt[i] );
+	for( int i = 1 ; i < pnt.size() ; ++i ){
+		while( l < i && pnt[i].st - pnt[l].st >= best )
+			box.erase( {pnt[l].nd, pnt[l].st} ), ++l;
+		auto D = box.upper_bound( {pnt[i].nd + d, pnt[i].st} );
+		for( auto it = box.lower_bound( {pnt[i].nd - d, pnt[i].st} ) ; it != D ; ++it ){
+			ll x = pnt[i].st-it->nd;
+			ll y = pnt[i].nd-it->st;
+			best = min( best, x*x + y*y );
+			d = ceil(sqrt(best));
+		}
+		box.insert( {pnt[i].nd, pnt[i].st} );
 	}
 	return best;
 }
