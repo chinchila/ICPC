69cec145898ef287c25f4f927106f7a021591e0b
João Sobral <pachecosobral@gmail.com>
2021-07-07 20:49:13 -0300

    NTT copypasta e ajeitando 128bit

--- code/misc/128bit.cpp
+++ code/misc/128bit.cpp
@@ -1,9 +1,10 @@
 __int128 input(){
     string s;
     cin >> s;
-    ll fst = (s[0] == '-') ? 1 : 0;
+    int fst = (s[0] == '-') ? 1 : 0;
     __int128 v = 0;
-    f(i,fst,s.size()) v = v * 10 + s[i] - '0';
+    for(int i = fst ; i < s.size() ; ++i)
+        v = v * 10 + s[i] - '0';
     if(fst) v = -v;
     return v;
 }
@@ -12,8 +13,8 @@ ostream& operator << (ostream& os,const __int128& v) {
     string ret, sgn;
     __int128 n = v;
     if(v < 0) sgn = "-", n = -v;
-    while(n) ret.pb(n % 10 + '0'), n /= 10;
-    reverse(all(ret));
+    while(n) ret.push_back(n % 10 + '0'), n /= 10;
+    reverse(ret.begin(), ret.end());
     ret = sgn + ret;
     os << ret;
     return os;
